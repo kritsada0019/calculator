9020bccbfb85b3066481b0138c320b4b7993dff7
krtsada <emmypinklab@gmail.com>
2018-02-21 11:43:15 +0700

    add complete

--- app/src/main/java/sdu/cs58/kritsada/calculator/MainActivity.java
+++ app/src/main/java/sdu/cs58/kritsada/calculator/MainActivity.java
@@ -2,12 +2,41 @@ package sdu.cs58.kritsada.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    EditText number1EditText, number2EditText;
+    TextView resultTextView;
+    Button addButton;
+    int num1,num2, result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Initial view
+        number1EditText = findViewById(R.id.edtNumber1);
+        number2EditText = findViewById(R.id.edtNumber2);
+        addButton = findViewById(R.id.btnAdd);
+        resultTextView = findViewById(R.id.txvResult);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //แปลงค่าที่รับจาก editTextเ ป็นตัวเลข
+                num1 = Integer.parseInt(number1EditText.getText().toString().trim());
+                num2 = Integer.parseInt(number2EditText.getText().toString().trim());
+                result = num1 + num2;
+                resultTextView.setText(result + "");
+            }
+        }); //end setOnclicklistener
+
+    }// end method onCreate
+}//end class
